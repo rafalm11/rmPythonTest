3b2f6a964e7c69a9c8d95c22bd8cae1caa71f81c
rafalm11 <133100213+rafalm11@users.noreply.github.com>
2023-07-16 14:24:34 +0200

    gitignore+positive test only

--- test_addOne.py
+++ test_addOne.py
@@ -5,11 +5,11 @@ print(addOne.addOne(2))
 def test_always_passes():
     assert True
 
-def test_always_fails():
-    assert False
+#def test_always_fails():
+#    assert False
 
 def test_addOne_positive():
     assert addOne.addOne(2)==3
 
-def test_addOne_negative():
-    assert addOne.addOne(5)==7
+#def test_addOne_negative():
+#    assert addOne.addOne(5)==7
